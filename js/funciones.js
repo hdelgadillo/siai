2892ca4aca6cde4b63d7e35da699e49bd08ce47c
hdelgadillo <33609841+hdelgadillo@users.noreply.github.com>
2018-05-22 11:25:49 -0500

    pagina
    pagina prototipo

---
+++ js/funciones.js
@@ -0,0 +1,25 @@
+﻿
+function preguntarSiNo(id){
+	alertify.confirm('Eliminar Datos', '¿Esta seguro de eliminar este registro?', 
+					function(){ eliminarDatos(id) }
+                , function(){ alertify.error('Se cancelo')});
+}
+
+function eliminarDatos(id){
+
+	cadena="id=" + id;
+
+		$.ajax({
+			type:"POST",
+			url:"php/eliminarDatos.php",
+			data:cadena,
+			success:function(r){
+				if(r==1){
+					alertify.success("Eliminado con exito!");
+					$("#div1").load("admin.php");
+				}else{
+					alertify.error("Fallo el servidor :(");
+				}
+			}
+		});
+}
---
+++ js/vendor/initializer.js
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.0.3/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDWvN0JXrintKr74hmq51XJXn8ZnUyjepg",
+    authDomain: "siai-43789.firebaseapp.com",
+    databaseURL: "https://siai-43789.firebaseio.com",
+    projectId: "siai-43789",
+    storageBucket: "siai-43789.appspot.com",
+    messagingSenderId: "723360820579"
+  };
+  firebase.initializeApp(config);
+</script>
